ae7b9645ec3d7397272448349f59301eb79a72fd
Jason-teamGravity <jason@teamgravity.in>
2025-12-05 21:45:22 +0530

    updated dashboard with base url

--- backend/server.js
+++ backend/server.js
@@ -30,14 +30,6 @@ const app = express();
 app.use(cors({ origin: true, credentials: true }));
 app.use(express.json());
 
-// Middleware to rewrite URL if it detects a double /api prefix
-app.use((req, res, next) => {
-  if (req.url.startsWith("/api/api/")) {
-    req.url = req.url.replace("/api/api", "/api");
-  }
-  next();
-});
-
 // ========================================================
 // SWAGGER SETUP
 // ========================================================
